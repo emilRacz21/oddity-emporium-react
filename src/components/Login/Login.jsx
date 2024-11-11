8ecdf8f3232233d193f8c5edac68e80a188fce5e
Emil Raczkiewicz <emil.raczkiewicz@gmail.com>
2024-11-11 12:05:15 +0100

    commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,25 @@
 import "./Login.scss";
+import key from "../../assets/svg/key.svg";
 import Navbar from "../Navbar/Navbar";
 export default function Login() {
   return (
     <Navbar title={["Getting started", "Login", "", "login"]}>
-      <section className="container">
-        <h2>login...</h2>
+      <section className="shop-container">
+        <img src={key} alt="Website logo" />
+        <section className="shop-section">
+          <h2>Please log in to your account!</h2>
+          <form>
+            <label>Your username</label>
+            <input type="text" />
+            <label>Your username</label>
+            <input type="text" />
+          </form>
+
+          <section className="shop-button">
+            <p>*All labels are requrired</p>
+            <button>Log in</button>
+          </section>
+        </section>
       </section>
     </Navbar>
   );
