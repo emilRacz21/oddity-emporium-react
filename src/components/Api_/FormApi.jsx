9cf9445fe79467d0992a9fbcb0d7c2bc59fd36eb
Emil Raczkiewicz <emil.raczkiewicz@gmail.com>
2024-11-12 18:42:43 +0100

    c

--- src/components/Api_/FormApi.jsx
+++ src/components/Api_/FormApi.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 export default function FormApi() {
-  const FORM_API = import.meta.env.VITE_FORM_API;
+  const FORM_API = `https://script.google.com/macros/s/${
+    import.meta.env.VITE_FORM_API
+  }/exec`;
   const formEle = document.querySelector("form");
   const formDatab = new FormData(formEle);
   axios.post(FORM_API, formDatab);
